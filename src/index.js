58649c77d3325f87f194a9935fd603963683fd5f
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-08-28 22:51:18 +0530

    router added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import {createRoot} from "react-dom/client";
 import Home from "./views/home/home";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
 const root = createRoot(document.getElementById("root"))
 root.render(<Home/>)
+const router =createBrowserRouter([{
+    path:'/',
+    element: <Home/>
+},{
+    path:'/about',
+    element:<About/>
+}])
