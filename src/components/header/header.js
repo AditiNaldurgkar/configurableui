314cbbe594fd00ede9e9d011a8236971e23d4e48
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-08-29 12:36:30 +0530

    header css changed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom";
 import "./header.css"
+import logo from "./logo.png"
 function Header(){
     return <div className="header">
+        <img  className="logo" src={logo}/>
         <h4>Hometown</h4>
+        <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+
+        </div>
     </div>
 }
 export default Header;
