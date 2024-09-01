761d8530bcb6a252077349dc3faed4c06f8bfac5
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-09-01 08:26:12 +0530

    review cards added

---
+++ src/components/reviewcards/reviewcards.js
@@ -0,0 +1,48 @@
+import "./reviewcards.css"
+import img3 from "./3-stars.png" 
+import img4 from "./4stars.png"
+import img5 from "./five-stars.png"
+const reviewCard = [
+    { name: "Radhika P", stars: 4, text: "Good quality of services" },
+    { name: "John D", stars: 5, text: "Nice and good quality furniture" },
+    { name: "Sarah W", stars: 3, text: "Beautiful designs" },
+    { name: "Maya D", stars: 4, text: "Comfortable and affordable decor" }
+  ];
+  
+  function Review({ name, stars, text }) {
+    let imgSrc;
+    switch (stars) {
+      case 3:
+        imgSrc = img3; 
+        break;
+      case 4:
+        imgSrc = img4; 
+        break;
+      case 5:
+        imgSrc = img5; 
+        break;
+    }
+  
+    return (
+      <div className="review-card">
+        <img src={imgSrc} alt={`${stars} stars`} />
+        <div>Name: {name}</div>
+        <div>{text}</div>
+      </div>
+    );
+  }
+  
+  function ReviewList() {
+    return (
+      <div>
+        {reviewCard.map((review, index) => (
+          <Review 
+            stars={review.stars}
+            name={review.name}
+            text={review.text}
+          />
+        ))}
+      </div>
+    );
+  }
+  export default ReviewList;
--- src/views/about/about.js
+++ src/views/about/about.js
@@ -1,8 +1,8 @@
 import "./about.css";
 import img from "./shop2.jpg"
 import Header from "../../components/header/header";
-
-
+import Footer from "../../components/footer/footer";
+import ReviewList from "../../components/reviewcards/reviewcards";
 function About() {
   return (
     <div className="abt-main">
@@ -15,6 +15,8 @@ function About() {
         src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14885.430003395652!2d79.0788107!3d21.1381665!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bd4c09182878f5b%3A0xa61e7391433285d8!2sHomeTown!5e0!3m2!1sen!2sin!4v1724867560475!5m2!1sen!2sin" 
       ></iframe>
       <img src={img} className="shopimg"></img>
+      <ReviewList/>
+      <Footer/>
     </div>
   );
 }
